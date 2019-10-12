746dedb9cb05a2c1cb65f6a426bdf3fdb834fa4d
Mariaelena <mariaelenasilveira@info.ufrn.br>
2019-10-12 10:01:35 -0300

    projeto no xcode iniciado

---
+++ Mônica/Mônica/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mônica
+//
+//  Created by Mariaelena Silveira on 12/10/19.
+//  Copyright © 2019 Mariaelena Silveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
